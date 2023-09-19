b8b97fa5af637eabb22449f4a4aaa9d1407da3e5
ts <>
2023-09-19 08:12:49 +0200

    Version

--- Eleu/LangServer/EleuLanguageServer.cs
+++ Eleu/LangServer/EleuLanguageServer.cs
@@ -28,7 +28,7 @@ record struct Cmd(string Name, object Arg)
 
 public class EleuLanguageServer : IDisposable
 {
-  public const string Version = "23.11";
+  public const string Version = "23.12";
   BlockingCollection<Cmd> queue = new(new ConcurrentQueue<Cmd>());
   Action<string, object> responseAction;
   Thread? worker;
