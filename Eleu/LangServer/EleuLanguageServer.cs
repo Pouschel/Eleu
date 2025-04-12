f19178327b6b84b7f0156b48fbc114edf10fa0a9
Thomas Steinfeld <steinfeld@mathepedia.de>
2025-04-12 21:28:03 +0200

    .net9

--- Eleu/LangServer/EleuLanguageServer.cs
+++ Eleu/LangServer/EleuLanguageServer.cs
@@ -29,7 +29,7 @@ record struct Cmd(string Name, object Arg)
 
 public class EleuLanguageServer : IDisposable
 {
-  public const string Version = "24.5";
+  public const string Version = "24.6";
   BlockingCollection<Cmd> queue = new(new ConcurrentQueue<Cmd>());
   Action<string, object> responseAction;
   Thread? worker;
