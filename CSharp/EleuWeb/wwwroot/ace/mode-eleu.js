693e6935f93c18d2cea377f2d873ec21d9d602ac
Thomas Steinfeld <steinfeld@mathepedia.de>
2025-05-23 10:27:08 +0200

    jsx removed

--- CSharp/EleuWeb/wwwroot/ace/mode-eleu.js
+++ CSharp/EleuWeb/wwwroot/ace/mode-eleu.js
@@ -385,9 +385,6 @@ ace.define("ace/mode/javascript_highlight_rules", ["require", "exports", "module
           defaultToken: "string.quasi"
         }]
       });
-
-      if (!options || options.jsx != false)
-        JSX.call(this);
     }
 
     this.embedRules(DocCommentHighlightRules, "doc-",
@@ -397,115 +394,6 @@ ace.define("ace/mode/javascript_highlight_rules", ["require", "exports", "module
   };
 
   oop.inherits(JavaScriptHighlightRules, TextHighlightRules);
-
-  function JSX()
-  {
-    var tagRegex = identifierRe.replace("\\d", "\\d\\-");
-    var jsxTag = {
-      onMatch: function (val, state, stack)
-      {
-        var offset = val.charAt(1) == "/" ? 2 : 1;
-        if (offset == 1)
-        {
-          if (state != this.nextState)
-            stack.unshift(this.next, this.nextState, 0);
-          else
-            stack.unshift(this.next);
-          stack[2]++;
-        } else if (offset == 2)
-        {
-          if (state == this.nextState)
-          {
-            stack[1]--;
-            if (!stack[1] || stack[1] < 0)
-            {
-              stack.shift();
-              stack.shift();
-            }
-          }
-        }
-        return [{
-          type: "meta.tag.punctuation." + (offset == 1 ? "" : "end-") + "tag-open.xml",
-          value: val.slice(0, offset)
-        }, {
-          type: "meta.tag.tag-name.xml",
-          value: val.substr(offset)
-        }];
-      },
-      regex: "</?" + tagRegex + "",
-      next: "jsxAttributes",
-      nextState: "jsx"
-    };
-    this.$rules.start.unshift(jsxTag);
-    var jsxJsRule = {
-      regex: "{",
-      token: "paren.quasi.start",
-      push: "start"
-    };
-    this.$rules.jsx = [
-      jsxJsRule,
-      jsxTag,
-      { include: "reference" },
-      { defaultToken: "string" }
-    ];
-    this.$rules.jsxAttributes = [{
-      token: "meta.tag.punctuation.tag-close.xml",
-      regex: "/?>",
-      onMatch: function (value, currentState, stack)
-      {
-        if (currentState == stack[0])
-          stack.shift();
-        if (value.length == 2)
-        {
-          if (stack[0] == this.nextState)
-            stack[1]--;
-          if (!stack[1] || stack[1] < 0)
-          {
-            stack.splice(0, 2);
-          }
-        }
-        this.next = stack[0] || "start";
-        return [{ type: this.token, value: value }];
-      },
-      nextState: "jsx"
-    },
-      jsxJsRule,
-    comments("jsxAttributes"),
-    {
-      token: "entity.other.attribute-name.xml",
-      regex: tagRegex
-    }, {
-      token: "keyword.operator.attribute-equals.xml",
-      regex: "="
-    }, {
-      token: "text.tag-whitespace.xml",
-      regex: "\\s+"
-    }, {
-      token: "string.attribute-value.xml",
-      regex: "'",
-      stateName: "jsx_attr_q",
-      push: [
-        { token: "string.attribute-value.xml", regex: "'", next: "pop" },
-        { include: "reference" },
-        { defaultToken: "string.attribute-value.xml" }
-      ]
-    }, {
-      token: "string.attribute-value.xml",
-      regex: '"',
-      stateName: "jsx_attr_qq",
-      push: [
-        { token: "string.attribute-value.xml", regex: '"', next: "pop" },
-        { include: "reference" },
-        { defaultToken: "string.attribute-value.xml" }
-      ]
-    },
-      jsxTag
-    ];
-    this.$rules.reference = [{
-      token: "constant.language.escape.reference.xml",
-      regex: "(?:&#[0-9]+;)|(?:&#x[0-9a-fA-F]+;)|(?:&[a-zA-Z0-9_:\\.-]+;)"
-    }];
-  }
 
   function comments(next)
   {
